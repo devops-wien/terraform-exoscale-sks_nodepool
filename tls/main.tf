6af04f860b2058c8892b2133ab56afa3ef3a6818
devops-wien <devops.wien@gmail.com>
2023-10-18 13:58:42 +0200

    Revert "EXO-5: [FIX] main.tf"
    This reverts commit afdb82cc8ccbbe5d492cd1665b5225b7582c8c63.

--- tls/main.tf
+++ tls/main.tf
@@ -4,7 +4,6 @@ resource "tls_private_key" "default" {
 }
 
 resource "tls_self_signed_cert" "default" {
-  key_algorithm   = tls_private_key.default.algorithm
   private_key_pem = tls_private_key.default.private_key_pem
 
   subject {
